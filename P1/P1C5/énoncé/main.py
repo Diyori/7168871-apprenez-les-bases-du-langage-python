d11bd617dd89b5e2db245cc77fa94a85639423a9
Diyori <diyoriyendountie@gmail.com>
2025-01-09 15:06:27 +0800

    Diyori.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "David"
+age = 27
+taille = 1.70
+est_etudiant = True
+
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
